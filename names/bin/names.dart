7279a71481ef840a1d281f9fde2a1fcdbed4e8ae
ummul fatima <ummulf02@gmail.com>
2026-01-04 17:07:21 +0500

    Add files via upload

---
+++ names/bin/names.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+import 'package:names/names.dart' as names;
+
+void main() {
+  // File file = new File(Directory.current.path + "\\names.txt");
+  // Future<String> p =  file.readAsString();
+
+  // p.then((data) => print(data));
+  // print("Main ends here");
+
+
+  
+}
---
+++ names/test/names_test.dart
@@ -0,0 +1,8 @@
+import 'package:names/names.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
