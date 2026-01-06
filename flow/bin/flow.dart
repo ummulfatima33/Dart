8bef644ee78b88aae1a3f506a61abea8d5b292b3
ummul fatima <ummulf02@gmail.com>
2026-01-06 16:13:57 +0500

    Add files via upload

---
+++ flow/bin/flow.dart
@@ -0,0 +1,40 @@
+// bin/flow.dart
+void main(){
+
+
+
+// for (int index=0; index < number.length;index++){
+//    print(number[index]);
+// }
+
+
+// for (var index in numbers) {
+//   print(index);
+// }
+
+
+//   for (int i = 1; i <= 100; i++) {
+//     if (i % 2 != 0) {
+//       print(i);
+//     }
+//   }
+
+
+  List<int> numbers = [110, 203, 350, 413, 50, 600, 70];
+
+  for (var num in numbers) {
+    if (num.isOdd) {
+      print(num);
+    }
+  }
+}
+
+
+
+
+
+
+ 
+
+
+
